1ff60f426c08e0e0d9ab90daeedd9fa44c8e2955
avikghosh9883-oss <avikghosh9883@gmail.com>
2025-08-11 00:25:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = API_ID
-API_HASH = "API_HASH"
+API_ID = 26701844
+API_HASH = "63ba937ff726aa65ef2650188dbcc891"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = "8322904378:AAEzEn9nkC6KcO4S7XbsHaswlQ-pFOOyFc8"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "MONGO_DB_URI"
+MONGO_DB_URI = "mongodb+srv://saderkhoje85: saderkhoje85@cluster0.caj5zho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = LOG_GROUP_ID
+LOG_GROUP_ID = -1002728135846
 
 # Get this value from @ultron2_robot on Telegram by /id
 OWNER_ID = 6874324360
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = "https://t.me/+BDWFAouuhW42YTE1"
-SUPPORT_GROUP = "https://t.me/+BDWFAouuhW42YTE1"
+SUPPORT_CHANNEL = "https://t.me/hiirukuuu"
+SUPPORT_GROUP = "https://t.me/hiirukuuu"
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -62,7 +62,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = "STRING_SESSION"
+STRING1 = "BQGXcBQAEc6Kca4VbOl2w8pNtoolo0AL0u5ECkdkz3r7Tc_JhGOuiT2bxLwa7lT_KAgeUJMOwhvvxg79ipEixfwJ-lYlcdYJzRUcDPZWMslHXBvUlrdCjkNZHhAqfxJRO2VF9ibInc5cU7k9RFwdM1yDxKKQan71Bzda8oZITtoOCj5CvohyLkKVCzS5BXsO1maIhmLxDc-ssx78TqM-80u-C6zyUj4WG8tyrb2HNMHy8JhH00J331XFx2yULiP_F3ftqOmJBDv-IXlyAc5CITzbHksywd23esjH4Ch9L9DKg96wlMg2xQaoZKcKU-GIIZsy_dRPc3cVIkoLR2W2KLNf9FnAAAAAHTcjdTAA"
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -81,9 +81,9 @@ START_IMG_URL = "https://graph.org/file/30e55c9dc1ae92736975a-0f9a77c6e91df04afb
 
 PING_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
 
-PLAYLIST_IMG_URL = "https://graph.org/file/763a841a2ad5cbb1e2fc5.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/30e55c9dc1ae92736975a-0f9a77c6e91df04afb.jpg"
 STATS_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
-TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/30e55c9dc1ae92736975a-0f9a77c6e91df04afb.jpg"
 TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
@@ -112,4 +112,5 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
